f77bd741565c6b1451156f75c745390ad33f92e2
horizon <1713113814@qq.com>
2020-07-04 21:05:19 +0800

    second commit second commit!

--- src/main/java/com/team5/bigprojects/controller/TestController.java
+++ src/main/java/com/team5/bigprojects/controller/TestController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.List;
 import java.util.Map;
 
@@ -21,7 +21,8 @@ public class TestController {
      * */
     @GetMapping("/total-info")
     @ResponseBody
-    public List<Map<String, Object>> get_atotall_info() {//男女购买商品的比率
+    public List<Map<String, Object>> get_atotall_info(@RequestParam(value = "flightNo")String flightNo) {//男女购买商品的比率
+        String sqlStr = "SELECT * FROM plane_log where flightNo = "+flightNo;
         return jdbcTemplate.queryForList("SELECT * FROM total_info");
     }
 }
--- src/main/java/com/team5/bigprojects/spark/SparkExample.java
+++ src/main/java/com/team5/bigprojects/spark/SparkExample.java
@@ -1,28 +1,28 @@
-package com.team5.bigprojects.spark;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-
-import java.util.Arrays;
-
-/**
- * @author lenovo
- */
-public class SparkExample {
-    public static void main(String[] args){
-        //1、创建spark的配置
-        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("SparkExample");
-        //2、根据配置生成spark的上下文
-        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-        //1、创建rdd(3种方法)
-        JavaRDD<Integer> rdd = ctx.parallelize(Arrays.asList(1,2,3,4,5));
-        //2、转化rdd,
-        JavaRDD<Integer>result = rdd.map(x->x*2);
-        //3、求值
-        result.foreach(x->System.out.println(x));
-        //1、关闭spark的上下文
-        ctx.close();
-
-    }
-}
+//package com.team5.bigprojects.spark;
+//
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.api.java.JavaRDD;
+//import org.apache.spark.api.java.JavaSparkContext;
+//
+//import java.util.Arrays;
+//
+///**
+// * @author lenovo
+// */
+//public class SparkExample {
+//    public static void main(String[] args){
+//        //1、创建spark的配置
+//        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("SparkExample");
+//        //2、根据配置生成spark的上下文
+//        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+//        //1、创建rdd(3种方法)
+//        JavaRDD<Integer> rdd = ctx.parallelize(Arrays.asList(1,2,3,4,5));
+//        //2、转化rdd,
+//        JavaRDD<Integer>result = rdd.map(x->x*2);
+//        //3、求值
+//        result.foreach(x->System.out.println(x));
+//        //1、关闭spark的上下文
+//        ctx.close();
+//
+//    }
+//}
