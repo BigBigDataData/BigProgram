4f326738112639e94f7be53c964401f86f41332b
zxl <1317367779@qq.com>
2020-07-07 18:15:13 +0800

    何时飞

--- src/main/java/com/team5/bigprojects/bean/WhenFlightDayQuery.java
+++ src/main/java/com/team5/bigprojects/bean/WhenFlightDayQuery.java
@@ -1,17 +1,38 @@
 package com.team5.bigprojects.bean;
 
-public class WhenFlightQuery {
+public class WhenFlightDayQuery {
+    @javax.validation.constraints.NotBlank(message = "出发日期不能为空,请输入")
+    private String month; //出发日期
+
     @javax.validation.constraints.NotBlank(message = "出发地点不能为空,请输入")
     private String dCity; //出发地
 
     @javax.validation.constraints.NotBlank(message = "目的地点不能为空,请输入")
     private String aCity; //目的地
 
-    public WhenFlightQuery() {
+
+    public WhenFlightDayQuery() {
     }
-    public WhenFlightQuery(WhenFlightQuery query){
+    public WhenFlightDayQuery(WhenFlightDayQuery query){
+        this.setMonth(query.getMonth());
         this.setdCity(query.getdCity());
         this.setaCity(query.getaCity());
+    }
+
+    public WhenFlightDayQuery(String month, String dCity, String aCity) {
+        this.month = month;
+        this.dCity = dCity;
+        this.aCity = aCity;
+    }
+
+
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String dDate) {
+        this.month= dDate;
     }
 
     public String getdCity() {
--- src/main/java/com/team5/bigprojects/controller/WhenFlightController.java
+++ src/main/java/com/team5/bigprojects/controller/WhenFlightController.java
@@ -1,9 +1,11 @@
 package com.team5.bigprojects.controller;
 
 import com.team5.bigprojects.bean.BasicQuery;
+import com.team5.bigprojects.bean.WhenFlightDayQuery;
 import com.team5.bigprojects.bean.WhenFlightQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +16,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Controller
 public class WhenFlightController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/round-trip-lea")
+    /**
+     *
+     * @param query
+     * @param bindingResult
+     * @return 何时飞 返回每月最低价
+     */
+    @GetMapping("/when-flight-month")
     @ResponseBody
-    public List<Map<String, Object>> get_roundtrip_lea_info(@Valid WhenFlightQuery query, BindingResult bindingResult) {
+    public List<Map<String, Object>> get_whenFlight_month_info(@Valid WhenFlightQuery query, BindingResult bindingResult) {
         List<Map<String, Object>> listMaps = new ArrayList<Map<String, Object>>();
         Map<String, Object> map1 = new HashMap<String, Object>();
         if(bindingResult.hasErrors()){
@@ -41,7 +49,68 @@ public class WhenFlightController {
             }
             return null;
         }
-        String sqlStr = "SELECT * FROM plane_log where dCity ="+query.getdCity()+"AND aCity = "+ query.getaCity() +"ORDER BY price";
+        String sqlStr = "SELECT price,month FROM plane_lowest_month WHERE dCity =" + query.getdCity()+ "AND aCity =" +query.getaCity()+ "ORDER BY month";
+        return jdbcTemplate.queryForList(sqlStr);
+    }
+
+    /**
+     *
+     * @param query
+     * @param bindingResult
+     * @return 返回当月每天最低价
+     */
+
+    @GetMapping("/when-flight-day-leave")
+    @ResponseBody
+    public List<Map<String, Object>> get_whenFlight_day_info(@Valid WhenFlightDayQuery query, BindingResult bindingResult) {
+        List<Map<String, Object>> listMaps = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map1 = new HashMap<String, Object>();
+        if(bindingResult.hasErrors()){
+            List<ObjectError> objectErrors = bindingResult.getAllErrors();
+            System.out.println(objectErrors.toString());
+            for(ObjectError objectError: objectErrors){
+                System.out.println("objectError = " + objectError.getObjectName());
+                //map1.put("resultName",objectError.getObjectName());
+                System.out.println("objectError = " + objectError.getDefaultMessage());
+                map1.put("resultMessage",objectError.getDefaultMessage());
+                System.out.println("objectError = " + objectError.getCode());
+                //map1.put("resultCode",objectError.getCode());
+                System.out.println("objectError = " + objectError.getArguments());
+                //map1.put("resultArguments",objectError.getArguments());
+                String str = objectError.getDefaultMessage();
+                //listMaps.add(map1)
+            }
+            return null;
+        }
+        String sqlStr = "SELECT price,dDate FROM plane_log_lowest_day WHERE" +query.getMonth()+ "AND dCity =" + query.getdCity()+ "AND aCity =" +query.getaCity()+ "ORDER BY dDate";
+
+        return jdbcTemplate.queryForList(sqlStr);
+    }
+
+
+    @GetMapping("/when-flight-day-back")
+    @ResponseBody
+    public List<Map<String, Object>> get_whenFlight_day_back_info(@Valid WhenFlightDayQuery query, BindingResult bindingResult) {
+        List<Map<String, Object>> listMaps = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map1 = new HashMap<String, Object>();
+        if(bindingResult.hasErrors()){
+            List<ObjectError> objectErrors = bindingResult.getAllErrors();
+            System.out.println(objectErrors.toString());
+            for(ObjectError objectError: objectErrors){
+                System.out.println("objectError = " + objectError.getObjectName());
+                //map1.put("resultName",objectError.getObjectName());
+                System.out.println("objectError = " + objectError.getDefaultMessage());
+                map1.put("resultMessage",objectError.getDefaultMessage());
+                System.out.println("objectError = " + objectError.getCode());
+                //map1.put("resultCode",objectError.getCode());
+                System.out.println("objectError = " + objectError.getArguments());
+                //map1.put("resultArguments",objectError.getArguments());
+                String str = objectError.getDefaultMessage();
+                //listMaps.add(map1)
+            }
+            return null;
+        }
+        String sqlStr = "SELECT price,dDate FROM plane_log_lowest_day WHERE" +query.getMonth()+ "AND dCity =" + query.getdCity()+ "AND aCity =" +query.getaCity()+ "ORDER BY dDate";
         return jdbcTemplate.queryForList(sqlStr);
     }
 
